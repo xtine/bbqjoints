17f98dd5bb869462e8290e3ef14498e7eac10e08
xtine <piroko@gmail.com>
2011-05-26 01:14:46 -0700

    fix raise404 import

--- joints/views.py
+++ joints/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
